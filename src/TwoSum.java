6f13c50b8f67a177bc7fb6ee785225eb0f54f66b
zalwata <zalwata@hotmail.com>
2021-02-16 15:33:17 -0800

    initial set up

---
+++ src/TwoSum.java
@@ -0,0 +1,19 @@
+/**
+ * Lab Assignment 2
+ */
+
+public class TwoSum {
+
+    public int[] twoSum(int[] arraySum, int intSum)
+    {
+
+
+        return arraySum;
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
